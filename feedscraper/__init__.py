c19a8786002706bae2688c2cebbed836abe754cd
Atai Ambus <atayambus+org@gmail.com>
2022-03-29 13:42:41 +0300

    fix(init): broken import

--- feedscraper/__init__.py
+++ feedscraper/__init__.py
@@ -1,5 +1,5 @@
 from feedscraper.feed import Feed, HomeFeed
 from feedscraper.post import Post
-from extractors import Field, Reaction
+from feedscraper.extractors import Field, Reaction
 
 __Version__ = '1.0.0'
